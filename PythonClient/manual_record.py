abcf8caf595d0c56ca738bf5f19bd659361ab6e6
wphkadn <wphkadn@163.com>
2018-08-09 10:35:55 +0800

    last version turbulence function not right. This time fix it. last vesion keyboard won't work, this fix it. Now joystick works not XBOX.

--- PythonClient/manual_record.py
+++ PythonClient/manual_record.py
@@ -88,7 +88,7 @@ NUM = 200
 CAM_WINDOW_WIDTH = 800
 CAM_WINDOW_HEIGHT = 600
 
-record_dir = '/home/kadn/data'
+record_dir = '../../data'
 
 
 def make_carla_settings(args):
@@ -130,50 +130,32 @@ def make_carla_settings(args):
     settings.add_sensor(lidar)
     return settings
 
-
-class Timer(object):
-    def __init__(self):
-        self.step = 0
-        self._lap_step = 0
-        self._lap_time = time.time()
-
-    def tick(self):
-        self.step += 1
-
-    def lap(self):
-        self._lap_step = self.step
-        self._lap_time = time.time()
-
-    def ticks_per_second(self):
-        return float(self.step - self._lap_step) / self.elapsed_seconds_since_lap()
-
-    def elapsed_seconds_since_lap(self):
-        return time.time() - self._lap_time
-
-
 def turbulence_halfsin(i, t, height=0.1):
     w = (np.pi/t)
     return height*np.sin(w*i)
+
+def turbulence_fullTriangle(i,t,height=0.1):
+    t = float(t)
+    if i <= t/4:
+        return 4*height/t*i
+    elif t/4 <= i <= 3*t/4:
+        return 2*height - 4*height/t*i
+    else:
+        return -4*height + 4*height/t*i
+
 
 
 class CarlaGame(object):
     def __init__(self, carla_client, args):
         self.client = carla_client
         self._carla_settings = make_carla_settings(args)
-        self._timer = None
         self._display = None
-        self._main_image = None
-        self._mini_view_image1 = None
-        self._mini_view_image2 = None
         self._enable_autopilot = False
-        self._lidar_measurement = None
-        self._map_view = None
         self._is_on_reverse = False
         self._display_map = args.map
         self._city_name = None
         self._map = None
         self._map_shape = None
-        self._map_view = None
         self._position = None
         self._agent_positions = None
 
@@ -186,15 +168,13 @@ class CarlaGame(object):
         self.targets_file = None
         self.index_file = 200
         self._command = 2
-        self.number_of_episodes = 1
-        self.frames_per_cut = 1800
+        self.number_of_episodes = 2
+        self.frames_per_cut = 4200
         self._joystick_control = 0
-        self._turbulence_start_flag = 0
         self._turbulence_start_frame = 0
         self._turbulence_stop_frame = 0
-        self._turbulence_len = 0
         self._turbulence = 0
-
+        self._turbulence_sym = 0
 
     def execute(self):
         """Launch the PyGame"""
@@ -206,7 +186,7 @@ class CarlaGame(object):
             print("Please make sure the joystick is on during the whole game!")
         else:
             self._joystick_control = 0
-            print("You can plug the joystick in, but it won't work!")
+            print("You'd better plug the joystick in to improve your driving experience!")
         for episode in range(self.number_of_episodes):
             self._creat_dir()
             self._initialize_game()
@@ -215,22 +195,29 @@ class CarlaGame(object):
                 if self._check_pygame() == False:
                     return
                 # add turbulence
-                if frame % 25 == 0:
-                    if np.random.rand() > 0.9:
+                if frame % 300 == 0:
+                    if np.random.rand() > 0.0:
+                        self._turbulence_sym = random.randrange(-1, 2, 2)
                         self._turbulence_start_frame = frame
-                        self._turbulence_stop_frame = frame + np.random.randint(13,24)
+                        self._turbulence_stop_frame = frame + np.random.randint(20,40)
                     else:
+                        self._turbulence_sym = 0
                         self._turbulence_start_frame = -1
                         self._turbulence_stop_frame = -1
-
+                if frame == self._turbulence_start_frame:
+                    print('turbulence start')
+                if frame == self._turbulence_stop_frame:
+                    print('turbulence stop')
                 if self._turbulence_start_frame < frame < self._turbulence_stop_frame :
-                    self._turbulence = random.randrange(-1, 2, 2)*turbulence_halfsin(frame - self._turbulence_start_frame,
-                                                                                    self._turbulence_stop_frame - self._turbulence_start_frame,
-                                                                                     height=0.1)
+                    self._turbulence = self._turbulence_sym * turbulence_fullTriangle(frame - self._turbulence_start_frame,
+                                                                                          self._turbulence_stop_frame - self._turbulence_start_frame,
+                                                                                          height=0.15)
                 else:
                     self._turbulence = 0
+                # if frame == 60:
+                    # print('Record process will start in 40 frames')
 
-                if frame % 4 == 0:
+                if frame % 10 == 0 and frame > 100:
                     self._on_loop(record=True, turbulence=self._turbulence)
                 else:
                     self._on_loop(record=False, turbulence=self._turbulence)
@@ -260,31 +247,24 @@ class CarlaGame(object):
 
     def _check_pygame(self):
         for event in pygame.event.get():
+            if self._joystick_control and event.type == pygame.JOYBUTTONUP and event.button == 0:
+                self._is_on_reverse = not self._is_on_reverse
             if event.type == pygame.QUIT:
                 return False
             if pygame.key.get_pressed()[K_ESCAPE]:
                 return False
-            if self._joystick_control and pygame.joystick.Joystick(0).get_button(5) and pygame.joystick.Joystick(0).get_button(7):
+            # if self._joystick_control and pygame.joystick.Joystick(0).get_button(5) and pygame.joystick.Joystick(0).get_button(7):
+            #     return False
+            if self._joystick_control and event.type == pygame.JOYBUTTONUP and event.button == 1:
                 return False
             return True
 
     def _initialize_game(self):
         self._on_new_episode()
-
-        if self._city_name is not None:
-            self._map = CarlaMap(self._city_name, 0.1643, 50.0)
-            self._map_shape = self._map.map_image.shape
-            self._map_view = self._map.get_map(WINDOW_HEIGHT)
-
-            extra_width = int((WINDOW_HEIGHT/float(self._map_shape[0]))*self._map_shape[1])
-            self._display = pygame.display.set_mode(
-                (WINDOW_WIDTH + extra_width, WINDOW_HEIGHT),
-                pygame.HWSURFACE | pygame.DOUBLEBUF)
-        else:
-            self._display = pygame.display.set_mode(
+        if self._joystick_control == 0:
+            pygame.display.set_mode(
                 (WINDOW_WIDTH, WINDOW_HEIGHT),
                 pygame.HWSURFACE | pygame.DOUBLEBUF)
-
         logging.debug('pygame started')
 
     def _on_new_episode(self):
@@ -295,19 +275,23 @@ class CarlaGame(object):
             self._city_name = scene.map_name
         number_of_player_starts = len(scene.player_start_spots)
         # player_start = np.random.randint(number_of_player_starts)
-        player_start = 85
+        player_start = 122
         print('Starting new episode...')
         self.client.start_episode(player_start)
-        self._timer = Timer()
         self._is_on_reverse = False
 
     def _on_loop(self,record = False, turbulence=0):
         measurements, sensor_data = self.client.read_data()
 
         if self._joystick_control == 1:
+            # control = self._get_XBOX_control(pygame.joystick.Joystick(0))
             control = self._get_joystick_control(pygame.joystick.Joystick(0))
         else:
             control = self._get_keyboard_control(pygame.key.get_pressed())
+
+        # this code means first record keyboard or joystick, then add turbulence to it, and send control to server.
+        if record :
+            self._on_record(measurements,sensor_data,control)
 
         if turbulence != 0:
             control.steer += turbulence
@@ -318,8 +302,6 @@ class CarlaGame(object):
             self.client.send_control(measurements.player_measurements.autopilot_control)
         else:
             self.client.send_control(control)
-        if record :
-            self._on_record(measurements,sensor_data,control)
 
     def _on_record(self, measurements, sensor_data, control):
         self._create_file()
@@ -343,6 +325,18 @@ class CarlaGame(object):
                 self.targets_file[self.index_file, 3] = control.hand_brake
                 self.targets_file[self.index_file, 4] = self._is_on_reverse
             self.index_file += 1
+
+    def _get_XBOX_control(self, joystick):
+        control = VehicleControl()
+        control.throttle = 0.5 * (joystick.get_axis(5) + 1)
+
+        control.brake = 0.5 * (joystick.get_axis(2) + 1)
+
+        control.steer = joystick.get_axis(0)
+        control.steer = 0.8 * control.steer
+
+        control.reverse = self._is_on_reverse
+        return control
 
     def _get_keyboard_control(self, keys):
         """
@@ -451,8 +445,8 @@ def main():
 
     logging.info('listening to server %s:%s', args.host, args.port)
 
-    print(__doc__)
-
+    # print(__doc__)
+    # os.system("gnome-terminal -e 'bash -c \"cd /home/kadn/AUTODRIVING/carla_python && ./CarlaUE4.sh -windowed -ResX=650 -ResY=350 -carla-server; exec bash\"'")
     while True:
         try:
 
--- PythonClient/testfunction/read.py
+++ PythonClient/testfunction/read.py
@@ -1,12 +1,11 @@
 import h5py
 import cv2
 
-f = h5py.File('/home/kadn/data/episode_010/data_000000.h5','r')
-num = 100
+f = h5py.File('../../data8_8/episode_001/data_000000.h5','r')
+
 for num in range(200):
     img=f['rgb'] [num,:,:,:]
     # img = np.split()
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     cv2.imshow('good',img)
     cv2.waitKey(0)
-# a.save('a.jpg')
